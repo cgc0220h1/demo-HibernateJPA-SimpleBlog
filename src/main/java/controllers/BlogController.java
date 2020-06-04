c52496e9d7d532a2b8d5bca3cd35f346baf71b9f
Duc <vanduc2514@gmail.com>
2020-06-05 03:32:41 +0700

    Thêm link cho phân trang

--- src/main/java/controllers/BlogController.java
+++ src/main/java/controllers/BlogController.java
@@ -4,6 +4,8 @@ import model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import service.IService;
@@ -23,19 +25,37 @@ public class BlogController {
         this.postService = postService;
     }
 
+    @ModelAttribute("numberOfPage")
+    public int numberOfPage() {
+        return postService.findPostPage(0, 6).getTotalPages();
+    }
+
     @GetMapping()
     public ModelAndView showHome() {
         ModelAndView modelAndView = new ModelAndView("index");
-        int numberOfPage = postService.findPostPage(0, 6).getTotalPages();
         List<Post> postList = postService.findPostByPage(0, 6);
+        modelAndView.addObject("numberOfPage");
+        List<Post> filterPost = summaryPost(postList);
+        modelAndView.addObject("postList", filterPost);
+        return modelAndView;
+    }
+
+    @GetMapping("/page{id}")
+    public ModelAndView switchPage(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Post> postList = postService.findPostByPage(id - 1, 6);
+        List<Post> filterPost = summaryPost(postList);
+        modelAndView.addObject("postList",filterPost);
+        return modelAndView;
+    }
+
+    private List<Post> summaryPost(List<Post> postList) {
         List<Post> filterPost = new LinkedList<>();
         for (Post post : postList) {
             post.setContent(truncateAfterWords(36, post.getContent()));
             filterPost.add(post);
         }
-        modelAndView.addObject("numberOfPage", numberOfPage);
-        modelAndView.addObject("postList", filterPost);
-        return modelAndView;
+        return filterPost;
     }
 
     private String truncateAfterWords(int numberOfWords, String string) {

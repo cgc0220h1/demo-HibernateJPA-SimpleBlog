2624cfca8e3818a2a1e8a9f59389182015976d43
Duc <vanduc2514@gmail.com>
2020-06-18 14:12:26 +0700

    Refactor

--- src/main/java/app/blog/config/AppConfiguration.java
+++ src/main/java/app/blog/config/AppConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,10 +13,6 @@ import app.blog.services.post.PostServiceImp;
 import app.blog.services.category.CategoryServiceImp;
 
 @Configuration
-//@EnableWebMvc
-//@ComponentScan({"app.blog.controllers", "app.blog.api"})
-//@EnableJpaRepositories("app.blog.repositories")
-@EnableSpringDataWebSupport
 public class AppConfiguration implements ApplicationContextAware, WebMvcConfigurer {
     private ApplicationContext applicationContext;
 
@@ -25,92 +20,6 @@ public class AppConfiguration implements ApplicationContextAware, WebMvcConfigur
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
-//
-//    @Bean
-//    public SpringResourceTemplateResolver templateResolver() {
-//        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-//        templateResolver.setApplicationContext(applicationContext);
-//        templateResolver.setPrefix("classpath:/templates/");
-//        templateResolver.setSuffix(".html");
-//        templateResolver.setTemplateMode(TemplateMode.HTML);
-//        templateResolver.setCharacterEncoding("UTF-8");
-//        return templateResolver;
-//    }
-//
-//    @Bean
-//    public SpringTemplateEngine templateEngine() {
-//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//        templateEngine.setTemplateResolver(templateResolver());
-//        templateEngine.addDialect(new LayoutDialect());
-//        templateEngine.addDialect(new SpringSecurityDialect());
-//        return templateEngine;
-//    }
-//
-//    @Bean
-//    public ThymeleafViewResolver viewResolver() {
-//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine(templateEngine());
-//        viewResolver.setCharacterEncoding("UTF-8");
-//        return viewResolver;
-//    }
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSource.setUrl("jdbc:mysql://localhost:3306/blog");
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("123456");
-//        return dataSource;
-//    }
-
-//    @Bean
-//    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
-//        return entityManagerFactory.createEntityManager();
-//    }
-//
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-//        entityManagerFactory.setDataSource(dataSource());
-//        entityManagerFactory.setPackagesToScan("app.blog.model");
-//        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-//        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
-//        entityManagerFactory.setJpaProperties(properties());
-//        return entityManagerFactory;
-//    }
-
-//    private Properties properties() {
-//        Properties properties = new Properties();
-//        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-//        properties.setProperty("hibernate.showSql", "true");
-//        return properties;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(entityManagerFactory);
-//        return transactionManager;
-//    }
-//
-//    @Override
-//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-//        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
-//        Pageable defaultPageable = PageRequest.of(0, 6, Sort.by("createTime").descending());
-//        resolver.setFallbackPageable(defaultPageable);
-//        resolver.setOneIndexedParameters(true);
-//        resolvers.add(resolver);
-//    }
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-//        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
-//        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
-//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-//    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -127,42 +36,4 @@ public class AppConfiguration implements ApplicationContextAware, WebMvcConfigur
         interceptor.setParamName("lang");
         registry.addInterceptor(interceptor);
     }
-
-//
-//
-//    @Bean
-//    public LocaleResolver localeResolver() {
-//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-//        localeResolver.setDefaultLocale(new Locale("vi", "VN"));
-//        return localeResolver;
-//    }
-//
-//    @Bean
-//    public MessageSource messageSource() {
-//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-//        messageSource.setBasename("locale/messages");
-//        messageSource.setDefaultEncoding("UTF-8");
-//        messageSource.setUseCodeAsDefaultMessage(true);
-//        return messageSource;
-//    }
-
-//    @Bean
-//    public PostService postService(PostRepository postRepository) {
-//        return new PostServiceImp(postRepository);
-//    }
-//
-//    @Bean
-//    public AuthorService authorService(AuthorRepository authorRepository) {
-//        return new AuthorServiceImp(authorRepository);
-//    }
-//
-//    @Bean
-//    public CategoryService categoryService(CategoryRepository categoryRepository) {
-//        return new CategoryServiceImp(categoryRepository);
-//    }
-//
-//    @Bean
-//    public CommentService commentService(CommentRepository commentRepository) {
-//        return new CommentServiceImp(commentRepository);
-//    }
 }
